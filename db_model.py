af850219e67a2e5e454e73c5de401c14f050b604
haze <hzmaxk@gmail.com>
2020-02-02 01:01:51 +0300

    feat: Модели с суффиксом 'C' для клиента

--- db_model.py
+++ db_model.py
@@ -2,16 +2,16 @@ from peewee import *
 from playhouse.sqlite_ext import SqliteExtDatabase, RowIDField
 from datetime import date
 
-def Init():
-    with db:
-        if (not db.table_exists('Chat')):
-            db.create_tables([Chat, User, Message])
-
 db = SqliteExtDatabase('peewee.db', pragmas=(
     ('cache_size', -1024 * 64),  # 64MB page-cache.
     ('journal_mode', 'wal'),  # Use WAL-mode (you should always use this!).
     ('foreign_keys', 1),
     ('c_extensions', True)))  # Enforce foreign-key constraints.
+
+def Init():
+    with db:
+        if (not db.table_exists('Chat')):
+            db.create_tables([Chat, User, Message, CUser, CToken])
 
 class BaseModel(Model):
     class Meta:
@@ -39,3 +39,19 @@ class Message(BaseModel):
     chat = ForeignKeyField(Chat, backref = 'messages'),
     state = IntegerField()
     content = TextField()
+
+class CUser(BaseModel):
+    rowid = RowIDField()
+    username = TextField()
+    password = TextField()
+    is_active = BooleanField()
+    is_telegramm_auth = BooleanField()
+    class Meta:
+        indexes = (
+            (('username', 'password'), True),
+        )
+
+class CToken(BaseModel):
+    rowid = RowIDField()
+    user = ForeignKeyField(CUser, backref = 'tokens'),
+    token = TextField()
